fe904dd53a75fa6c565dac9ba4025e6312e6c885
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-01-17 14:27:18 -0300

    feat(Add): product box styles and background styles

--- frontend-tech-code/src/components/Layout/styles.ts
+++ frontend-tech-code/src/components/Layout/styles.ts
@@ -5,18 +5,76 @@ export const Container = styled.div`
   flex-direction: column;
   width: 100vw;
   height: 100vh;
-  background-color: #252525;
+  background-color: #222222;
+  padding: 2.5rem;
 `;
 
 export const Box = styled.div`
-  display: flex;
-  width: 80vw;
-  height: 80vh;
-  background-color: #424242;
-  align-items: center;
-  justify-content: center;
-  border-radius: 14px;
-  margin: 0 auto;
-  margin-top: 60px;
-  padding: 1.5rem 1rem;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, auto));
+  gap: 1.75rem;
+  background
+
+  @media (max-width: 500px) {
+    grid-template-columns: 1fr;
+  }
+  
+  .product {
+    position: relative;
+    background: #181818;
+    padding: 1.75rem 1.5rem;
+    border-radius: 4px;
+
+    h2 {
+      margin-bottom: 0.75rem;
+      font-weight: 700;
+      font-size: 1.5rem;
+      text-align: center;
+    }
+
+    img {
+      object-fit: cover;
+      width: 100%;
+      height: 11.25rem;
+      border-radius: 4px;
+      margin-bottom: 0.375rem;
+    }
+
+    p {
+      font-size: 0.875rem;
+    }
+
+    div {
+      margin-top: 0.875rem;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      strong {
+        font-size: 2rem;
+        font-weight: 500;
+      }
+
+      button {
+        background: darkred;
+        width: 3rem;
+        height: 3rem;
+        border: none;
+        border-radius: 50%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        svg {
+          stroke: white;
+          width: 1.5rem;
+          height: 1.5rem;
+        }
+
+        &:hover {
+        background: red;
+        }
+      }
+    }
+  }
 `;
