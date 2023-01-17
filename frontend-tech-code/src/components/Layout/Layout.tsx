2e81343ea84e7984ae583f21e528ff43cf9d510c
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-01-17 13:27:10 -0300

    feat(Add): background-color on layout and content box

---
+++ frontend-tech-code/src/components/Layout/Layout.tsx
@@ -0,0 +1,13 @@
+import { Box, Container } from './styles'
+
+const Layout = () => {
+  return (
+    <Container>
+      <Box>
+
+      </Box>
+    </Container>
+  )
+}
+
+export default Layout
---
+++ frontend-tech-code/src/components/Layout/styles.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+  background-color: #252525;
+`;
+
+export const Box = styled.div`
+  display: flex;
+  width: 80vw;
+  height: 80vh;
+  background-color: #424242;
+  align-items: center;
+  justify-content: center;
+  border-radius: 14px;
+  margin: 0 auto;
+  margin-top: 60px;
+  padding: 1.5rem 1rem;
+`;
