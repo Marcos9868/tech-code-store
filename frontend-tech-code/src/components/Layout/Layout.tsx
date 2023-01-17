671cf872da1fc77efa3e4c6f8d76b74ec18dd75d
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-01-17 14:28:02 -0300

    feat(Add): estructure that will render the products of api

--- frontend-tech-code/src/components/Layout/Layout.tsx
+++ frontend-tech-code/src/components/Layout/Layout.tsx
@@ -1,10 +1,71 @@
 import { Box, Container } from './styles'
+import { FiPlus } from 'react-icons/fi';
+import { currencyFormat } from '../../helpers/currency';
 
 const Layout = () => {
+  const products = [
+    {
+      id: 1,
+      name: 'GeForce GTX 770 Ti',
+      image: '',
+      description: '',
+      price: 500,
+    },
+    {
+      id: 1,
+      name: 'GeForce GTX 770 Ti',
+      image: '',
+      description: '',
+      price: 500,
+    },
+    {
+      id: 1,
+      name: 'GeForce GTX 770 Ti',
+      image: '',
+      description: '',
+      price: 500,
+    },
+    {
+      id: 1,
+      name: 'GeForce GTX 770 Ti',
+      image: '',
+      description: '',
+      price: 500,
+    },
+    {
+      id: 1,
+      name: 'GeForce GTX 770 Ti',
+      image: '',
+      description: '',
+      price: 500,
+    },
+    {
+      id: 1,
+      name: 'GeForce GTX 770 Ti',
+      image: '',
+      description: '',
+      price: 500,
+    },
+  ];
+
   return (
     <Container>
       <Box>
-
+        {!products.length
+          ? [1, 2, 3, 4].map((n) => <p>Loading...</p>)
+          : products.map((product) => (
+            <div key={product.id} className='product'>
+              <h2>{product.name}</h2>
+              <img src={product.image} alt={product.image} />
+              <p>{product.description}</p>
+              <div>
+                <strong>{currencyFormat(product.price)}</strong>
+                <button type='button'>
+                  <FiPlus />
+                </button>
+              </div>
+            </div>
+          ))}
       </Box>
     </Container>
   )
