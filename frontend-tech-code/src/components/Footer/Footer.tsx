6eb0353be9bbcf6d315a259d83953888d6314e62
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-01-17 13:28:59 -0300

    fix: change background color and display

---
+++ frontend-tech-code/src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Container } from './styles'
+
+const Footer = () => {
+  return (
+    <Container>
+      <h3>Developed by Marcos M. Ferreira</h3>
+      <p>Todos os direitos reservados</p>
+    </Container>
+  )
+}
+
+export default Footer
---
+++ frontend-tech-code/src/components/Footer/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 70px;
+  align-items: center;
+  justify-content: center;
+  background-color: #181818;
+  color: #f5f5f5;
+`;
