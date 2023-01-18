de4baf37c90dd0dc4811db96adfa50194262971d
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-01-18 11:04:25 -0300

    feat(Add): preset of Storage page

--- frontend-tech-code/src/pages/Storage/Storage.tsx
+++ frontend-tech-code/src/pages/Storage/Storage.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import Footer from '../../components/Footer/Footer'
+import Header from '../../components/Header/Header'
+import StorageBox from '../../components/Storage/StorageBox'
+import { Container } from './styles'
 
 const Storage = () => {
   return (
-    <div>Storage</div>
+    <Container>
+      <Header />
+      <StorageBox />
+      <Footer />
+    </Container>
+    
   )
 }
 
