6aba86d79e78adc2dc7c194e675fc5db14ecd6ca
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-01-18 15:35:42 -0300

    feat(Add): storage routes

--- frontend-tech-code/src/App.tsx
+++ frontend-tech-code/src/App.tsx
@@ -1,5 +1,9 @@
 import { ChakraProvider } from "@chakra-ui/react"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Create from "./components/Storage/Create/Create"
+import ListStorage from "./components/Storage/ListStorage/ListStorage"
+import Remove from "./components/Storage/Remove/Remove"
+import Update from "./components/Storage/Update/Update"
 import Home from "./pages/Home/Home"
 import Login from "./pages/Login/Login"
 import Storage from "./pages/Storage/Storage"
@@ -12,7 +16,12 @@ function App() {
           <Route path="/">
             <Route path="/" element={<Login />}/>
             <Route path="/home" element={<Home />}/>
-            <Route path="/storage" element={<Storage />}/>
+            <Route path="/storage" element={<Storage />}>
+              <Route path="/storage/list" element={<ListStorage />} />
+              <Route path="/storage/create" element={<Create />} />
+              <Route path="/storage/update" element={<Update />}/>
+              <Route path="/storage/remove" element={<Remove />}/>
+            </Route>
           </Route>
         </Routes>
       </ChakraProvider>
