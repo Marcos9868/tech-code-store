3bf866156b1309b425c4fcb5ba8b50142f5c918f
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-01-18 15:36:46 -0300

    fix: Turn button component dinamic

--- frontend-tech-code/src/components/Card/Button/Button.tsx
+++ frontend-tech-code/src/components/Card/Button/Button.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@chakra-ui/react'
 
-const DButton = () => {
+const DButton = ({ children }: any) => {
   return (
     <Button
       loadingText='Loading'
@@ -9,7 +9,7 @@ const DButton = () => {
       spinnerPlacement='start'
       _hover={{ bg: '#f2f2f2', color: 'black'}}
     >
-      Entrar
+      { children }
     </Button>
   )
 }
