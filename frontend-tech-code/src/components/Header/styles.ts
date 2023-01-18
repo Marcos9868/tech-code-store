4f4975f614357faff7b83c6c590b85cab30f325f
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-01-18 10:59:04 -0300

    feat(Add): storage button styles

--- frontend-tech-code/src/components/Header/styles.ts
+++ frontend-tech-code/src/components/Header/styles.ts
@@ -6,7 +6,17 @@ export const Container = styled.div`
   width: 100vw;
   height: 6rem;
   align-items: center;
-  justify-content: center;
+  justify-content: space-around;
+
+  button {
+    background-color: #181818;
+    color: #f5f5f5;
+    border: none;
+    outline: none;
+    border-radius: 5px;
+    width: 5rem;
+    height: 3rem;
+  }
 
   @media (max-width: 720px) {
     display: flex;
