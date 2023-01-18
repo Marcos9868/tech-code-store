302f83a95500bf9d47ea1253746d10aadfb2bf23
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-01-18 15:43:30 -0300

    feat(Add): estructure of product controller

--- tech-code-api/src/controllers/products.controller.ts
+++ tech-code-api/src/controllers/products.controller.ts
@@ -1,5 +1,25 @@
-class ProductsController {
+import { Request, Response } from "express";
 
+class ProductsController {
+  async create(req: Request, res: Response) {
+
+  }
+
+  async list(req: Request, res: Response) {
+
+  }
+
+  async show(req: Request, res: Response) {
+
+  }
+
+  async update(req: Request, res: Response) {
+
+  }
+
+  async remove(req: Request, res: Response) {
+
+  }
 }
 
 export default new ProductsController();
