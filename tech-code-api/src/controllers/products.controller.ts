0e490c4d7a67303690f8aba041547efa09691d88
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-01-18 18:05:18 -0300

    feat(Add): create, list and show methods

--- tech-code-api/src/controllers/products.controller.ts
+++ tech-code-api/src/controllers/products.controller.ts
@@ -1,16 +1,22 @@
 import { Request, Response } from "express";
+import ProductsRepository from '../database/repositories/products.repository';
 
 class ProductsController {
   async create(req: Request, res: Response) {
-
+    const incoming = req.body;
+    const newProduct = await ProductsRepository.createProduct(incoming);
+    res.status(201).send(newProduct);
   }
 
   async list(req: Request, res: Response) {
-
+    const products = await ProductsRepository.listProducts();
+    res.status(200).send(products);
   }
 
   async show(req: Request, res: Response) {
-
+    const name = req.params.name;
+    const product = await ProductsRepository.getProduct(name);
+    res.status(200).send(product)
   }
 
   async update(req: Request, res: Response) {
