f85146d07ecf3302edaf87c11500c692fa490ddc
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-01-18 12:15:30 -0300

    feat(Add): base of queries and scripts to db

---
+++ tech-code-api/src/database/repositories/products.repository.ts
@@ -0,0 +1,26 @@
+import db from "../config";
+import Product from "../../models/product.model";
+
+class ProductsRepository {
+  async createProduct() {
+
+  }
+
+  async listProducts() {
+
+  }
+
+  async getProduct() {
+
+  }
+
+  async updateProduct() {
+
+  }
+
+  async removeProduct() {
+
+  }
+}
+
+export default new ProductsRepository();
