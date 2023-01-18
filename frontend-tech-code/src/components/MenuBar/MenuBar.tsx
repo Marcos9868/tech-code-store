d43f44b1c61b8a64e903ff9c53af2774c4da9fe0
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-01-18 10:55:17 -0300

    feat(Add): menu bar of storage

---
+++ frontend-tech-code/src/components/MenuBar/MenuBar.tsx
@@ -0,0 +1,35 @@
+import { NavLink } from 'react-router-dom'
+import { Container } from './styles'
+
+const MenuBar = () => {
+  return (
+    <Container>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/">
+              <span>Listar Produtos</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="pizzas">
+              <span>Cadastro</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="drinks">
+              <span>Modificar Produto</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="ice-creams">
+              <span>Remover Produto</span>
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </Container>
+  )
+}
+
+export default MenuBar;
