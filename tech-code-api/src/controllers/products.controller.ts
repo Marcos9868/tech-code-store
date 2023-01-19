142bc6e43107d3d1d6afaf2d5b6550ae93357c67
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-01-19 14:20:44 -0300

    fix: name param to id, feat(Add): update and remove controller methods

--- tech-code-api/src/controllers/products.controller.ts
+++ tech-code-api/src/controllers/products.controller.ts
@@ -14,17 +14,33 @@ class ProductsController {
   }
 
   async show(req: Request, res: Response) {
-    const name = req.params.name;
-    const product = await ProductsRepository.getProduct(name);
+    const idProduct = req.params.id;
+    const product = await ProductsRepository.getProduct(parseInt(idProduct));
     res.status(200).send(product)
   }
 
   async update(req: Request, res: Response) {
+    try {
+      const idProduct = req.params.id;
+      const modifiedProduct = req.body;
+      modifiedProduct.id = idProduct;
+
+      await ProductsRepository.updateProduct(modifiedProduct);
+      res.status(200).send(modifiedProduct);
+    } catch (error) {
+      res.status(400).json({ error: 'Failed to change product information' });
+    }
 
   }
 
   async remove(req: Request, res: Response) {
-
+    try {
+      const { id, name } = req.body;
+      await ProductsRepository.removeProduct(id, name);
+      res.status(200)
+    } catch (error) {
+      res.status(400)
+    }
   }
 }
 
