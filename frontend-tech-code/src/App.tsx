2c271bbea905da893b697ebd31aff3273f45fb87
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-01-26 15:47:07 -0300

    fix: remove storage and login routes

--- frontend-tech-code/src/App.tsx
+++ frontend-tech-code/src/App.tsx
@@ -1,12 +1,6 @@
-import { ChakraProvider } from "@chakra-ui/react"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Create from "./components/Storage/Create/Create"
-import ListStorage from "./components/Storage/ListStorage/ListStorage"
-import Remove from "./components/Storage/Remove/Remove"
-import Update from "./components/Storage/Update/Update"
-import Home from "./pages/Home/Home"
-import Login from "./pages/Login/Login"
-import Storage from "./pages/Storage/Storage"
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
@@ -14,14 +8,7 @@ function App() {
       <ChakraProvider>
         <Routes>
           <Route path="/">
-            <Route path="/" element={<Login />}/>
-            <Route path="/home" element={<Home />}/>
-            <Route path="/storage" element={<Storage />}>
-              <Route path="/storage/list" element={<ListStorage />} />
-              <Route path="/storage/create" element={<Create />} />
-              <Route path="/storage/update" element={<Update />}/>
-              <Route path="/storage/remove" element={<Remove />}/>
-            </Route>
+            <Route path="/" element={<Home />}/>
           </Route>
         </Routes>
       </ChakraProvider>
