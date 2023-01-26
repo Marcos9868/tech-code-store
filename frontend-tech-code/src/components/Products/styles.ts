2a1c75e691c89daaeb379f4a40a3be7db5f6009f
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-01-26 17:08:40 -0300

    feat(Add): styles of products component

---
+++ frontend-tech-code/src/components/Products/styles.ts
@@ -0,0 +1,75 @@
+import styled from "styled-components";
+
+export const Box = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, auto));
+  gap: 1.75rem;
+  background
+
+  @media (max-width: 500px) {
+    grid-template-columns: 1fr;
+  }
+  
+  .product {
+    position: relative;
+    background: #181818;
+    padding: 1.75rem 1.5rem;
+    border-radius: 4px;
+
+    h2 {
+      margin-bottom: 0.75rem;
+      font-weight: 700;
+      font-size: 1.5rem;
+      text-align: center;
+      color: #f5f5f5
+    }
+
+    img {
+      object-fit: cover;
+      width: 100%;
+      height: 11.25rem;
+      border-radius: 4px;
+      margin-bottom: 0.375rem;
+    }
+
+    p {
+      font-size: 0.875rem;
+      color: #f5f5f5;
+      margin-top: 0.75rem;
+    }
+
+    div {
+      margin-top: 0.875rem;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      strong {
+        font-size: 2rem;
+        font-weight: 500;
+        color: #f5f5f5;
+      }
+
+      button {
+        background: darkred;
+        width: 3rem;
+        height: 3rem;
+        border: none;
+        border-radius: 50%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        svg {
+          stroke: white;
+          width: 1.5rem;
+          height: 1.5rem;
+        }
+
+        &:hover {
+        background: red;
+        }
+      }
+    }
+  }
+`;
