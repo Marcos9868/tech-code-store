cd9172e4312d7bd13243beecc56aa76c30317379
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-01-26 17:10:29 -0300

    feat(Add): Menu bar on home page

--- frontend-tech-code/src/pages/Home/Home.tsx
+++ frontend-tech-code/src/pages/Home/Home.tsx
@@ -1,12 +1,14 @@
 import Footer from '../../components/Footer/Footer'
 import Header from '../../components/Header/Header'
 import Layout from '../../components/Layout/Layout'
+import MenuBar from '../../components/MenuBar/MenuBar'
 import { Container } from './styles'
 
 const Home = () => {
   return (
     <Container>
       <Header />
+      <MenuBar />
       <Layout />
       <Footer />
     </Container>
